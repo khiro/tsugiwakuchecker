8ba6a7ed5b868bca72bda4e0f2bdb384c3515b59
Hiroki Kuzuno <kuzuno@gmail.com>
2011-01-13 05:22:58 +0900

    updates url on first opened tab

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
-var req = new XMLHttpRequest();
-var timer = null;
 var currentwatchurl = null;
 var cachewatchurl = null;
+var timer = null;
+var currentwindow = null;
+var currenttab = null;
 
 function xhr(comurl, callback) {
+    var req = new XMLHttpRequest();
     req.onerror = function(error) {
         console.dir(error);
     }
@@ -15,19 +17,20 @@ function xhr(comurl, callback) {
         var div = document.createElement("div");
         div.innerHTML = req.responseText;
         var r = div.getElementsByClassName("r");
-        if (r) {
+        if (r.length != 0) {
             var nextwatchurl = r[0].childNodes[0].href;
+            if (nextwatchurl == undefined)
+                return
             if (currentwatchurl.match('co[0-9]+')) {
                 if (cachewatchurl == null) {
                     cachewatchurl = nextwatchurl;
-                } else if (nextwatchurl != cachewatchurl) {
+                } 
+                else if (nextwatchurl != cachewatchurl) {
                     callback(nextwatchurl);
-                    currentwatchurl = nextwatchurl;
                 }
             }
             else if (nextwatchurl != currentwatchurl) {
                 callback(nextwatchurl);
-                currentwatchurl = nextwatchurl;
             }
         }
     };
@@ -41,13 +44,17 @@ chrome.extension.onRequest.addListener(
             clearInterval(timer);
             cachewatchurl = null;
             currentwatchurl = request.watchurl;
+            chrome.windows.getCurrent(function(w) {
+                currentwindow = w;
+                chrome.tabs.getSelected(currentwindow.id, function(t) {
+                    currenttab = t;
+                });
+            });
+            xhr(request.comurl, function() {} );
             timer = setInterval(function() {
                 xhr(request.comurl, function(nextwatchurl) {
-                    chrome.windows.getCurrent(function(w) {
-                        chrome.tabs.getSelected(w.id, function(t) {
-                            chrome.tabs.update(t.id, {url: nextwatchurl});
-                        });
-                    });
+                    chrome.tabs.update(currenttab.id, {url: nextwatchurl});
+                    currentwatchurl = nextwatchurl;
                 });
             }, 60000);
         }
--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 function sendRequest() {
     var shosai = document.getElementsByClassName("shosai");    
     var url = shosai[0].childNodes[4].href || shosai[0].childNodes[6].href;
-    
+    var windowurl = location.href.split('?')[0];
     chrome.extension.sendRequest({action: 'nico', comurl: url,
-                                  watchurl: location.href.split('?')[0]},
+                                  watchurl: windowurl},
                                  function(response) {
                                       alert(response.result);
                                   });
